359aa4723b30e68f7c579875a688b7349af10596
ghghwldk <ghghwldk@gmail.com>
2023-07-22 14:46:55 +0900

    first commit the spring project

---
+++ src/main/kotlin/com/kotlinsecurity/converted/ConvertedApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlinsecurity.converted
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConvertedApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConvertedApplication>(*args)
+}
---
+++ src/test/kotlin/com/kotlinsecurity/converted/ConvertedApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlinsecurity.converted
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConvertedApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
